0652e2e757af8902f48f97c0cf468ed87c7a5bad
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-12-20 10:19:24 -0500

    refactor code so that view renders based on model in state.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-console.log("hello");
-
 let board = new Array(64);
+
+let currentPlayer = '';
+
+let prevPlayer = '';
+
+let turn = 0;
+
+let winner = '';
+
 let boardEls = [];
 
 const boardContainerEl = document.getElementById("board-container");
@@ -10,31 +17,69 @@ const testEl = document.getElementById("test");
 init();
 
 function init() {
+  initBoardState();
+  initPieceState();
   setupBoard();
 }
 
-function setupBoard() {
-  for (i = 1; i <= board.length; i++) {
-    // create the tiles of the board
-    let newSquare = document.createElement("div");
-    newSquare.setAttribute("class", "square");
-    if (i <= 8 ||
-        i >= 17 && i <= 24 ||
-        i >= 33 && i <= 40 ||
-        i >= 49 && i <= 56) {
+function initBoardState() {
+  for (i = 0; i < board.length; i++) {
+    if (
+      i < 8 ||
+      i > 15 && i < 24 ||
+      i > 31 && i < 40 ||
+      i > 47 && i < 56
+    ) {
       if (i % 2 !== 0) {
-      newSquare.setAttribute("black", "false");
+        board[i] = '';
       } else {
-        newSquare.setAttribute("black", "true");
+        board[i] = null;
       }
     } else {
       if (i % 2 === 0) {
-        newSquare.setAttribute("black", "false");
+        board[i] = ''
       } else {
-        newSquare.setAttribute("black", "true");
+        board[i] = null;
       }
     }
-    newSquare.textContent = "";
+  }
+  console.log(board);
+}
+
+function initPieceState() {
+  for (i = 0; i < board.length; i++) {
+    if (i < 24 && board[i] !== null) {
+      board[i] = 'X';
+    }
+    if (i > 32 && board[i]!== null) {
+      board[i] = 'O';
+    }
+  }
+  console.log(board);
+}
+
+function setupBoard() {
+  for (i = 0; i < board.length; i++) {
+      // create the tiles of the board
+      let newSquare = document.createElement("div");
+      newSquare.setAttribute("class", "square");
+      // set the tiles' colour
+      if (board[i] !== null) {
+        newSquare.setAttribute("black", "true");
+      } else {
+        newSquare.setAttribute("black", "false");
+      }
+      // set up the pieces
+      if (board[i] === 'X') {
+        newSquare.textContent === 'X';
+      }
+      if (board[i] === 'O') {
+        newSquare.textContent === 'O';
+      }
+      // add event listeners
+      if (board[i] === 'X' || board[i] === 'O') {
+
+      }
     boardContainerEl.appendChild(newSquare);
     boardEls.push(newSquare);
   }
