83baf7f53798b9571b20882ae33f270b1197fa65
Michael Baldanza <michael.snider.baldanza@gmail.com>
2021-01-08 13:49:55 -0500

    change css, reorganize code, add movement capability for red's first turn

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
-let board = new Array(64);
+/*----- constants -----*/
+const board = new Array(64);
 
-let redPieces = 0;
-let blackPieces = 0;
+const moves = {
+  men: [9, 7],
+  king: [9, 7, -9, -7,] 
+};
+
+/*----- app's state (variables) -----*/
+let moveablePieces = [];
+let possibleMoves = [];
 
 let boardEls = [];
-let redPieceEls = [];
-let blackPieceEls = [];
 let pieceEls = [];
 
 let turnCounter = 1;
-let currentPlayer = turnCounter % 2 !== 0 ? 'b' : 'r';
+let currentPlayer = turnCounter % 2 !== 0 ? 'r' : 'w';
 
 const boardContainerEl = document.getElementById('board-container');
-
-const testEl = document.getElementById('test');
 
 init();
 
 function init() {
   initBoardState();
   initPieceState();
-  setupBoard();
+  setupBoardView();
+  setupPieceView();
   startGame();
 }
 
 function initBoardState() {
   for (i = 0; i < board.length; i++) {
+    // separate elements based on whether they are playable (empty strings) or
+    // non-playable (null)
     if (
       i < 8 ||
       i > 15 && i < 24 ||
@@ -50,89 +56,121 @@ function initBoardState() {
 
 function initPieceState() {
   for (i = 0; i < board.length; i++) {
+    // add text to elements that represent starting positions of red and black
     if (i < 24 && board[i] !== null) {
       board[i] = 'r';
-      redPieces++;
     }
     if (i > 39 && board[i]!== null) {
-      board[i] = 'b';
-      blackPieces++;
+      board[i] = 'w';
     }
   }
   console.log(board);
-  console.log(`Red has ${redPieces} on the board`);
-  console.log(`Black has ${blackPieces} on the board`)
 }
 
-function setupBoard() {
+function setupBoardView() {
   for (i = 0; i < board.length; i++) {
-    // create the tiles of the board
+    // create tiles of the board
     let newSquare = document.createElement('div');
-    let newTileNo = i.toString();
-    // add tracking attributes
+    // add attributes for CSS
     newSquare.setAttribute('class', 'square');
-    newSquare.setAttribute('tileNo', newTileNo);
-    // set the tiles' colour
+    newSquare.setAttribute('tileNo', i.toString());
     if (board[i] !== null) {
-      newSquare.setAttribute('black', 'true');
+      newSquare.setAttribute('grey', 'true');
     } else {
-      newSquare.setAttribute('black', 'false');
+      newSquare.setAttribute('grey', 'false');
     }
-    // set up the pieces
-    if (board[i] === 'r') {
-      let newRedPiece = document.createElement('div');
-      newRedPiece.setAttribute('class', 'piece');
-      newRedPiece.setAttribute('red', 'true');
-      newRedPiece.setAttribute('tileNo', newTileNo);
-      newSquare.appendChild(newRedPiece);
-      redPieceEls.push(newRedPiece);
-      pieceEls.push(newRedPiece);
-    }
-    if (board[i] === 'b') {
-      let newBlackPiece = document.createElement('div');
-      newBlackPiece.setAttribute('class', 'piece');
-      newBlackPiece.setAttribute('red', 'false');
-      newBlackPiece.setAttribute('tileNo', newTileNo);
-      newSquare.appendChild(newBlackPiece);
-      blackPieceEls.push(newBlackPiece);
-      pieceEls.push(newBlackPiece);
-    }
+    // lay the tiles on the board
     boardContainerEl.appendChild(newSquare);
+    // add the tiles to an array for ease of 'moving' pieces
     boardEls.push(newSquare);
   }
-  console.log(`The board view is manifested by ${boardEls}`);
+}
+
+function setupPieceView() {
+  for (i = 0; i < board.length; i++) {
+    // create pieces
+    let newPiece = document.createElement('div');
+    // add identifying attribute
+    newPiece.setAttribute('class', 'piece');
+    newPiece.setAttribute('tileNo', i.toString());
+    if (board[i] === 'r') {
+      // red tiles
+      newPiece.setAttribute('red', 'true');
+      boardEls[i].appendChild(newPiece);
+      pieceEls.push(newPiece);
+    } else if (board[i] === 'w') {
+      // black tiles
+      newPiece.setAttribute('class', 'piece');
+      newPiece.setAttribute('red', 'false');
+      boardEls[i].appendChild(newPiece);
+      pieceEls.push(newPiece)
+    } else {
+      // add null elements so that pieceEls tracks board
+      let emptyTile = null;
+      pieceEls.push(emptyTile);
+    }
+  }
 }
 
 function startGame() {
-  takeTurn();
+  determineMovement();
 }
 
-function takeTurn() {
+function determineMovement() {
   // determine what pieces can move
-  let moveablePieces = [];
-  console.log(`The current player is ${currentPlayer}`)
   for (i = 0; i < board.length; i++) {
     // select the indices modeling the current player's pieces
     if (board[i] === currentPlayer) {
-      // select the piece that can make valid move
-      if (
-        board[i - 9] === '' ||
-        board[i - 7] === '' ||
-        board[i + 7] === '' ||
-        board[i + 9] === ''
-      ) { 
-        // change the model to reflect that the piece is allowed to be moved
-        moveablePieces.push(i);
-        board[i] = 'm';
+      // toggle the red pieces that can make a valid move
+      if (currentPlayer === 'r') {
+        if (board[i + moves.men[0]] === '' || board[i + moves.men[1]] === '') {
+          pieceEls[i].addEventListener('click', selectPiece);
+          board[i] = 'm';
+        }   
+      }
+      // toggle the white pieces that can make a valid move
+      if (currentPlayer === 'w') {
+        if (board[i - moves.men[0]] === '' || board[i - moves.men[1]] === '') {
+          pieceEls[i].addEventListener('click', selectPiece);
+          board[i] = 'm';
+        }
       }
     }
   }
-  moveablePieces.forEach(function(piece) {
-    console.log(`The tile at ${piece} contains ${board[piece]}`);
-  });
-  console.log(`${moveablePieces} are moveable pieces.`);
-  // add event listeners to the moveable pieces
-  // for (i = 0; i < board.length; i++) {
-  //   if (board)
-  // }
+}
+
+function selectPiece(evt) {
+  let pieceId = evt.target.getAttribute('tileNo');
+  board[pieceId] = 's';
+  for (i = 0; i < board.length; i++) {
+    if (board[i] === 'm') {
+      pieceEls[i].removeEventListener('click', selectPiece);
+      board[i] = currentPlayer;
+    }
+    if (currentPlayer === 'r') {
+      if (board[i] === 's') {
+        board[i + moves.men[0]] = 'd';
+        board[i + moves.men[1]] = 'd';
+      }
+    }
+    if (currentPlayer === 'w') {
+      if (board[i] === 's') {
+        board[i - moves.men[0]] = 'd';
+        board[i - moves.men[1]] = 'd';
+      }
+    }
+    if (board[i] === 'd') {
+      boardEls[i].addEventListener('click', selectDestination);
+    }
+  }
+  console.log(pieceEls);
+}
+
+function selectDestination(evt) {
+  alert(`${evt.target.getAttribute('tileNo')} has been selected!`);
+  console.log(board.indexOf('s'));
+  evt.target.appendChild(pieceEls[board.indexOf('s')]);
+  for (i = 0; i < board.length; i++) {
+
+  }
 }
