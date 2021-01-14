17a7768b65fb15371ea42e096f254fca1e79eca5
Michael Baldanza <michael.snider.baldanza@gmail.com>
2021-01-14 09:10:14 -0500

    fix up piece movement

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const board = new Array(64);
 
 const moves = {
   men: [9, 7],
+  jump: [17, 14], 
   king: [9, 7, -9, -7,] 
 };
 
@@ -14,6 +15,7 @@ let boardEls = [];
 
 let turnCounter = 0;
 let currentPlayer;
+let prevPlayer;
 
 const boardContainerEl = document.getElementById('board-container');
 
@@ -96,8 +98,11 @@ function setupBoardView() {
 function takeTurn() {
   turnCounter += 1;
   currentPlayer = turnCounter % 2 !== 0 ? 'r' : 'w';
+  prevPlayer = turnCounter % 2 === 0 ? 'r' : 'w';
   console.log(turnCounter);
   console.log(currentPlayer);
+  console.log(`This is the board at the beginning of a player's turn`);
+  console.log(board);
   determineMovement();
 }
 
@@ -107,48 +112,75 @@ function determineMovement() {
     if (board[i] === currentPlayer) {
       // toggle the red pieces that can make a valid move
       if (currentPlayer === 'r') {
-        if (board[i + moves.men[0]] === '' || board[i + moves.men[1]] === '') {
+        if (
+          board[i + moves.men[0]] === '' ||
+          board[i + moves.men[1]] === '' ||
+          board[i + moves.men[0]] === 'w' && board[i + moves.jump[0]] === '' ||
+          board[i + moves.men[1]] === 'w' && board[i + moves.jump[1]] === ''
+        ) {
           boardEls[i].firstChild.addEventListener('click', selectPiece);
           board[i] = 'm';
         }   
       }
       // toggle the white pieces that can make a valid move
       if (currentPlayer === 'w') {
-        if (board[i - moves.men[0]] === '' || board[i - moves.men[1]] === '') {
+        if (
+          board[i - moves.men[0]] === '' ||
+          board[i - moves.men[1]] === '' ||
+          board[i - moves.men[0]] === 'r' && board[i - moves.jump[0]] === '' ||
+          board[i - moves.men[1]] === 'r' && board[i - moves.jump[1]] === ''
+        ) {
           boardEls[i].firstChild.addEventListener('click', selectPiece);
           board[i] = 'm';
         }
       }
     }
   }
+  console.log(`This is the board after running determineMovement`);
+  console.log(board)
 }
 
 function selectPiece(evt) {
   let selectedPiece = evt.target;
   selectedPiece.setAttribute('selected', '');
   let pieceId = selectedPiece.parentElement.getAttribute('tileNo');
+  console.log(boardEls[pieceId]);
   board[pieceId] = 's';
   selectedPiece.removeEventListener('click', selectPiece);
   for (i = 0; i < board.length; i++) {
+    let redMove1 = i + moves.men[0];
+    let redMove2 = i + moves.men[1];
+    let whiteMove1 = i - moves.men[0];
+    let whiteMove2 = i - moves.men[1];
     if (board[i] === 'm') {
       boardEls[i].firstChild.removeEventListener('click', selectPiece);
       board[i] = currentPlayer;
     }
     if (board[i] === 's') {
       if (currentPlayer === 'r') {
-        board[i + moves.men[0]] = 'd';
-        board[i + moves.men[1]] = 'd';
-        boardEls[i + moves.men[0]].addEventListener('click', selectDestination);
-        boardEls[i + moves.men[1]].addEventListener('click', selectDestination);
+        // if (board[i + moves.men[0]])
+        if (board[redMove1] === '') {
+          board[redMove1] = 'd';
+          boardEls[redMove1].addEventListener('click', selectDestination);
+        }
+        if (board[redMove2] === '') {
+          board[redMove2] === 'd';
+          boardEls[redMove2].addEventListener('click', selectDestination);
+        }
       }
       if (currentPlayer === 'w') {
-        board[i - moves.men[0]] = 'd';
-        board[i - moves.men[1]] = 'd';
-        boardEls[i - moves.men[0]].addEventListener('click', selectDestination);
-        boardEls[i - moves.men[1]].addEventListener('click', selectDestination);
+        if(board[whiteMove1] === '') {
+          board[whiteMove1] = 'd';
+          boardEls[whiteMove1].addEventListener('click', selectDestination);
+        }
+        if(board[whiteMove2] === '') {
+          board[whiteMove2] = 'd';
+          boardEls[whiteMove2].addEventListener('click', selectDestination);
+        }
       }
     }
   }
+  console.log(`This is the board after selectPiece`);
   console.log(board);
 }
 
@@ -156,6 +188,7 @@ function selectDestination(evt) {
   let selectedDestination = evt.target;
   let newIdx = parseInt(selectedDestination.getAttribute('tileNo'));
   let oldIdx = board.indexOf('s');
+  console.log(`${currentPlayer} moves from ${oldIdx} to ${newIdx}`);
   let selectedPiece = boardEls[oldIdx].firstChild;
   // move piece
   selectedDestination.appendChild(selectedPiece);
