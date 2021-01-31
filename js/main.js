5f0becb732710f27ddc76c5f3204fd6968f63fbb
Michael Baldanza <michael.snider.baldanza@gmail.com>
2021-01-31 13:16:42 -0500

    remove commented out code

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ function initView() {
 function render() {
   for (i = 0; i < board.length; i++) {
     let piece = squares[i].firstChild
-    // if (board[i] === 's') piece.setAttribute('color', 'yellow');
     if (Math.round(board[i]) === 1) piece.setAttribute('color', 'red');
     if (Math.round(board[i]) === -1) piece.setAttribute('color', 'white');
     if (!Number.isInteger(board[i]) && typeof(board[i]) === 'number') {
@@ -63,6 +62,7 @@ function render() {
 }
 
 function takeTurn() {
+  console.log(board);
   turn *= -1;
   canJump = [];
   canMove = [];
@@ -108,37 +108,22 @@ function selectPiece(evt) {
     canJump.indexOf(selPieceIdx) !== -1 ||
     canMove.indexOf(selPieceIdx) !== -1
   ) {
-    // board[sqIdx] = 's';
     resetStrings();
     render();
     setJump(selPieceIdx);
     if (canMove.length) setMove();
   }
-  // if (typeof(board[sqIdx]) !== 'string') return;
-  // board[sqIdx] = 's';
-  // resetStrings();
-  // render();
-  // setMove();
-  // setJump();
+  console.log(board);
 }
 
 function setMove() {
-  // let selPieceIdx = board.indexOf('s');
   availMoves = checkMove(selPieceIdx, moves.men);
   console.log(availMoves);
-  // availMoves.forEach(function (move) {
-  //   board[move] = 'd';
-  // })
 }
 
 function setJump(idx) {
-  // let selPieceIdx = board.indexOf('s');
   availJumps = checkJump(idx, moves.men, moves.jump);
   if (availJumps) {
-    // availJumps.forEach(function (pair) {
-    //   board[pair[0]] = 'q';
-    //   board[pair[1]] = 'd';
-    // });
     mustJump = true;
   }
 }
@@ -169,25 +154,6 @@ function selectDest(evt) {
       endTurn(newIdx);
     }
   }
-  // let selDest = evt.target;
-  // let newIdx = Number(selDest.getAttribute('tileNo'));
-  // if (board[newIdx] !== 'd') return;
-  // let oldIdx = board.indexOf('s');
-  // board[oldIdx] = 0;
-  // if (getMoveType(oldIdx, newIdx)) {
-  //   endTurn(newIdx);
-  // } else {
-  //   board[newIdx] = 's';
-  //   resetStrings();
-  //   render();
-  //   mustJump = false;
-  //   setJump();
-  //   if (mustJump) {
-  //     return;
-  //   } else {
-  //     endTurn(newIdx);
-  //   }
-  // }
 }
 
 function getBoard() {
@@ -246,10 +212,6 @@ function checkJump(idx, moveArr, jumpArr) {
       board[idx + turn * moveArr[l]] === turn * -1 &&
       board[idx + turn * jumpArr[l]] === 0
     ) {
-      // let quarry = idx + turn * moveArr[l];
-      // let dest = idx + turn * jumpArr[l];
-      // let specJump = [quarry, dest]
-      // jumps.push(specJump);
       jumps.push(idx + turn * jumpArr[l])
     }
   return jumps;
